6ba01cb1bb3f8f370617267a0b150c3977d3a6cb
Omar Essilfie-Quaye <omareq08@gmail.com>
2018-08-20 15:00:01 +0100

    added bouncing ball code

---
+++ bball/README.md
@@ -0,0 +1,5 @@
+# Love Potion Code
+
+Basically a race with a friend of mine to see who could make a coubcing ball simulation the fastest.  Check out the [Demo](https://omareq.github.io/bball)
+
+Created using [p5.js](https://p5js.org/)
---
+++ bball/ball.js
@@ -0,0 +1,45 @@
+
+class Ball {
+	constructor(x, y, vx, vy, r, m) {
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+		this.r = r;
+		this.m = m;	
+	}
+
+	applyForce(fx, fy, dt) {
+		let ax = fx/this.m;
+		this.vx += ax * dt;
+		this.x += this.vx * dt;
+
+		let ay = fy/this.m;
+		this.vy += ay * dt;
+		this.y += this.vy * dt;
+	}
+
+	checkEdges() {
+		if(this.x + this.r > width) {
+			this.x = width - this.r;
+			this.vx *= -1;
+		}
+		if(this.y + this.r > height) {
+			this.y = height - this.r;
+			this.vy *= -1;
+		}
+		if(this.x - this.r < 0) {
+			this.x = this.r;
+			this.vx *= -1;
+		}
+		if(this.y - this.r < 0) {
+			this.y = this.r;
+			this.vy *= -1;
+		}
+	}
+
+	show() {
+		fill(225);
+		ellipse(this.x, height - this.y, 2 * this.r, 2 * this.r);
+	}
+}
