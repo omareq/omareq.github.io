82801a81b808afbfc6f815aeac0e389ba6c9ae22
Omar Essilfie-Quaye <omareq08@gmail.com>
2020-08-22 19:12:00 +0100

    Created quadruped project

--- quadruped/sketch.js
+++ quadruped/sketch.js
@@ -10,6 +10,46 @@
  ******************************************************************************/
 
 /**
+ * Instance of Robot class
+ *
+ * @type       {Robot}
+ */
+let natalya;
+
+/**
+ * Angle of rotation of the robot on imaginary platform.
+ *
+ * @type       {number}
+ */
+let viewAngle = 0;
+
+let i = 0;
+let lastState = 1;
+let firstState = 0;
+let state = firstState;
+
+walk_array = [0.1, 0, 0,
+                0, 0, 0,
+                0, 0, 0];
+
+/**
+ * Function to delay execution of the current thread by the time provided.
+ *
+ * @param      {number}  milliseconds  The delay period in milliseconds.
+ */
+function delay(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+function preload() {
+  natalya = new Robot(1000);
+}
+
+/**
  * p5.js setup function, creates canvas.
  */
 function setup() {
@@ -19,14 +59,35 @@ function setup() {
 	} else {
 		cnvSize = windowWidth;
 	}
-	let cnv = createCanvas(cnvSize, 0.7 * cnvSize);
+	let cnv = createCanvas(cnvSize, 0.7 * cnvSize, WEBGL);
 	cnv.parent('sketch');
+    console.log("Set up Complete");
 }
 
 /**
  * p5.js draw function, is run every frame to create the desired animation
  */
 function draw() {
-	background(0);
+    background(250);
+    // model(objModel);
+    viewAngle += radians(0.5);
+    fill(0);
+    if(state == 0) {
+        natalya.home(i);
+
+    } else if(state == 1) {
+        natalya.stand90(i);
+    }
+
+    i+=0.01;
+
+    if(i >= 1) {
+        i = 0;
+        state++;
+        if(state > lastState) {
+            state = firstState;
+        }
+    }
+    natalya.draw(0,0,0, -PI/6, viewAngle,0);
 }
 
