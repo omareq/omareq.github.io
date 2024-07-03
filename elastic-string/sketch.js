2b9e31a43057b351131f4d86c69c41e15110bee0
Omar Essilfie-Quaye <omareq08@gmail.com>
2024-07-02 17:39:56 -0700

    elastic-string: init commit

---
+++ elastic-string/sketch.js
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ *
+ *	@file sketch.js A quick simulation to test elasticity
+ *
+ *	@author Omar Essilfie-Quaye <omareq08+githubio@gmail.com>
+ *	@version 1.0
+ *	@date 02-July-2024
+ *	@link https://omareq.github.io/elastic-string/
+ *	@link https://omareq.github.io/elastic-string/docs/
+ *
+ *******************************************************************************
+ *
+ *                   GNU General Public License V3.0
+ *                   --------------------------------
+ *
+ *   Copyright (C) 2024 Omar Essilfie-Quaye
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+"use strict";
+
+/**
+ * p5.js setup function, creates canvas.
+ */
+function setup() {
+	let cnvSize;
+	if(windowWidth > windowHeight) {
+		cnvSize = windowHeight;
+	} else {
+		cnvSize = windowWidth;
+	}
+	let cnv = createCanvas(cnvSize, 0.7 * cnvSize);
+	cnv.parent('sketch');
+}
+
+/**
+ * p5.js draw function, is run every frame to create the desired animation
+ */
+function draw() {
+	background(0);
+}
+
---
+++ elastic-string/string.js
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *
+ *  @file string.js A quick simulation to test elasticity
+ *
+ *  @author Omar Essilfie-Quaye <omareq08+githubio@gmail.com>
+ *  @version 1.0
+ *  @date 02-July-2024
+ *  @link https://omareq.github.io/elastic-string/
+ *  @link https://omareq.github.io/elastic-string/docs/
+ *
+ *******************************************************************************
+ *
+ *                   GNU General Public License V3.0
+ *                   --------------------------------
+ *
+ *   Copyright (C) 2024 Omar Essilfie-Quaye
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+
+
+
