50b6e9740eff27b1da37406f57088bbf9850e750
Omar Essilfie-Quaye <omareq08@gmail.com>
2025-03-23 23:14:42 -0700

    tanks: projectile debug mode fire all projectile types at different powers

--- tanks/mode-debug-projectile.js
+++ tanks/mode-debug-projectile.js
@@ -1,6 +1,35 @@
-
-
-
+/*******************************************************************************
+ *
+ *  @file mode-debug-projectile.js A file to test projectile motion
+ *
+ *  @author Omar Essilfie-Quaye <omareq08+githubio@gmail.com>
+ *  @version 1.0
+ *  @date 12-February-2025
+ *  @link https://omareq.github.io/tanks/
+ *  @link https://omareq.github.io/tanks/docs/
+ *
+ *******************************************************************************
+ *
+ *                   GNU General Public License V3.0
+ *                   --------------------------------
+ *
+ *   Copyright (C) 2025 Omar Essilfie-Quaye
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+"use strict";
 
 /**
  * Class empty mode that doesn't do anything.
@@ -9,16 +38,21 @@
  */
 TankGame.ModeList.DebugProjectile = class extends TankGame.Mode {
     /**
-     * Calls super() and exits
+     * Constructor for the Debug projectile mode.
      */
     constructor() {
         super();
         console.debug("Start the Debug Projectile Mode");
         this.startSpeed = 100;
+        this.keys = Object.keys(TankGame.ProjectileParamList);
+        this.keysIndex = 0;
     };
 
     /**
-     * exits immediately
+     * Update the projectile debug mode - adds new projectile if there are none
+     * currently in the screen
+     *
+     * @param (number) dt - The time between the current frame and the previous frame.
      */
     update(dt) {
         if(this.gameEngine.projectiles.length==0) {
@@ -27,13 +61,19 @@ TankGame.ModeList.DebugProjectile = class extends TankGame.Mode {
             this.startSpeed -= 10;
             if(this.startSpeed <= 10) {
                 this.startSpeed = 100;
+                this.keysIndex++;
+                if(this.keysIndex > this.keys.length - 1) {
+                    this.keysIndex = 0;
+                }
             }
             const bearing = 45;
+            const key = this.keys[this.keysIndex];
             const testProjectile = new TankGame.Projectile(
                 startPos,
                 speed,
                 bearing,
-                TankGame.ProjectileParamList.SmallMissile);
+                TankGame.ProjectileParamList[key]);
+            console.debug("Add ", key, " to the projectiles list.");
 
             this.gameEngine.addProjectile(testProjectile);
         }
---
+++ tanks/world.js
@@ -0,0 +1,3 @@
+// ground
+
+// wind
