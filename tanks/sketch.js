e3f0da127ce6486040e11010c7810ebf5a6761b5
Omar Essilfie-Quaye <omareq08@gmail.com>
2025-09-26 17:19:17 -0700

    tanks: WIP debug player

--- tanks/sketch.js
+++ tanks/sketch.js
@@ -46,7 +46,7 @@ function setup() {
 	let cnv = createCanvas(cnvSize, 0.7 * cnvSize);
 	cnv.parent('sketch');
 
-	tanksGameEngine = new TankGame.GameEngine(new TankGame.ModeList.DebugTank());
+	tanksGameEngine = new TankGame.GameEngine(new TankGame.ModeList.DebugPlayer());
 }
 
 /**
