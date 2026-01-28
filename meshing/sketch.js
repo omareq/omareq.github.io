9c5b10cd02795666aa96a25a9b5e709ebb724f6b
Omar Essilfie-Quaye <omareq08@gmail.com>
2026-01-27 20:09:13 -0800

    meshing: fixed linting issue - assignment to functionparam for shuffle array in place function

--- meshing/sketch.js
+++ meshing/sketch.js
@@ -211,7 +211,8 @@ function wheelSpokes(myHull) {
 }
 
 
-function shuffleArrayInPlace(array) {
+function shuffleArray(originalAarray) {
+  let array = [...originalAarray];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
     [array[i], array[j]] = [array[j], array[i]]; // Swap elements
@@ -262,7 +263,7 @@ function draw() {
 	drawHull(myHull, 0);
 	mesh.draw();
 	if(myPoints.length) {
-		shuffleArrayInPlace(myPoints);
+		myPoints = shuffleArray(myPoints);
 		const nextVertex = new Vertex(myPoints[0].x, myPoints[0].y);
 		push();
 		strokeWeight(2);
