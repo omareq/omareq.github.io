f2d89efea13f293ce80fd8965e8af83587635e0a
Omar Essilfie-Quaye <omareq08@gmail.com>
2026-02-08 19:28:55 -0800

    bf: skip running same program twice if it is unchanged

--- bf-interpreter/sketch.js
+++ bf-interpreter/sketch.js
@@ -52,9 +52,14 @@ function run() {
 	const programTxt = preProcess(rawProgram);
 	const program = new BFProgram(parse(programTxt));
 
+	if(lastProgram != undefined && programTxt.join("") == lastProgram.join("")) {
+		console.log("Same program not running again.");
+		return;
+	}
+
 	let cpu = new BFCpu(8, program, 30000);
 	cpu.execute();
-	lastProgram = program;
+	lastProgram = programTxt;
 }
 
 /**
