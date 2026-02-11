76d33dedce25f40cb19544a64f0318a0018e9d76
Omar Essilfie-Quaye <omareq08@gmail.com>
2026-02-10 22:13:49 -0800

    bf: add input EOF test

--- test/p_032.js
+++ test/p_032.js
@@ -3,11 +3,13 @@ const {parse, BFProgram} = require("../bf-interpreter/bf-program.js");
 
 function output() {} // Inject empty dependency for the BF CPU
 
-QUnit.test("p_032 BF Interpreter: Hello Test", function(assert) {
+QUnit.module("p_032", function (hooks) {
+
+QUnit.test("BF Interpreter: Hello Test", function(assert) {
     assert.ok(1 == "1", "Passed!" );
 });
 
-QUnit.test("p_032 BF Interpreter Test: +", function(assert) {
+QUnit.test("BF Interpreter: +", function(assert) {
     const inputAsciiCodes = [0];
     const program = new BFProgram(parse("+"));
     const memSize = 1;
@@ -17,7 +19,7 @@ QUnit.test("p_032 BF Interpreter Test: +", function(assert) {
     assert.equal(cpu.data[0], 1, "BF Interpreter Test: +");
 });
 
-QUnit.test("p_032 BF Interpreter Test: Hello World", function(assert) {
+QUnit.test("BF Interpreter: Hello World", function(assert) {
     const programText = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++.>+++++++++++++++.>.+++.------.--------.";
     const inputAsciiCodes = [0];
     const program = new BFProgram(parse(programText));
@@ -28,7 +30,7 @@ QUnit.test("p_032 BF Interpreter Test: Hello World", function(assert) {
     assert.equal(cpu.outputBuffer.join(""), "Hello World", "BF Interpreter Test: Hello World");
 });
 
-QUnit.test("p_032 BF Interpreter Test: Print A-Z", function(assert) {
+QUnit.test("BF Interpreter: Print A-Z", function(assert) {
     const programText = "++++++[->++++++++++<]>+++++<++++++++++++++++++++++++++[->.+<]";
     const inputAsciiCodes = [0];
     const program = new BFProgram(parse(programText));
@@ -39,7 +41,20 @@ QUnit.test("p_032 BF Interpreter Test: Print A-Z", function(assert) {
     assert.equal(cpu.outputBuffer.join(""), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "BF Interpreter Test: Print A-Z");
 });
 
-QUnit.test("p_032 BF Interpreter Test: Echo Input", function(assert) {
+QUnit.test("BF Interpreter: Input EOF", function(assert) {
+    const programText = ",,";
+    const inputAsciiCodes = [65];
+    const program = new BFProgram(parse(programText));
+    const memSize = 1;
+    let cpu = new BFCpu(8, program, memSize, inputAsciiCodes, output);
+    cpu.step();
+    assert.equal(cpu.data[0], 65, "BF Interpreter Test: Echo Input");
+    cpu.step();
+    assert.equal(cpu.data[0], 0, "BF Interpreter Test: Echo Input");
+    // console.log(cpu);
+});
+
+QUnit.test("BF Interpreter: Echo Input", function(assert) {
     const programText = ",[.,]";
     const inputAsciiCodes = [65, 66, 67, 68, 69];
     const program = new BFProgram(parse(programText));
@@ -50,7 +65,7 @@ QUnit.test("p_032 BF Interpreter Test: Echo Input", function(assert) {
     assert.equal(cpu.outputBuffer.join(""), "ABCDE", "BF Interpreter Test: Echo Input");
 });
 
-QUnit.test("p_032 BF Interpreter Test: Reverse Input", function(assert) {
+QUnit.test("BF Interpreter: Reverse Input", function(assert) {
     const programText = ">,[>,]<[.<]";
     const inputAsciiCodes = [65, 66, 67, 68, 69];
     const program = new BFProgram(parse(programText));
@@ -62,4 +77,5 @@ QUnit.test("p_032 BF Interpreter Test: Reverse Input", function(assert) {
 });
 
 
+});
 
